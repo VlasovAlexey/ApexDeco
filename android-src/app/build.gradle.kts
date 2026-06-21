bad8aa08c404644daa737018f7b53fedabb2e02d
Alexey Vlasov <a.vlasov@gaijin.team>
2026-06-21 20:43:28 +0300

    minSdk = 26 now

--- android-src/app/build.gradle.kts
+++ android-src/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.avlasov.apexdeco"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 35
         versionCode = 2
         versionName = "1.1"
